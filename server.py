b6ee30a8aa00180d5aba254d6be4af1edf6ea487
hellsp <hellsp@hotmail.com>
2019-11-10 18:09:59 +1100

    update test result

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ class Employees_Name(Resource):
 class ShowTest(Resource):
     def get(self):
         f = open('test', 'r')
-        r = f.readline().strip()
+        r = f.readlines().strip()
         f.close()
         return (r)
         # result = ([{'line': t, 'idmap': None, 'frequency': None} for t in r])
