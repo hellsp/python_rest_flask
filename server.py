4fc2d95c16f6ce1d8ae1e55624c8aa7f7f9eb56d
hellsp <hellsp@hotmail.com>
2019-11-10 16:22:38 +1100

    fix read file json

--- server.py
+++ server.py
@@ -61,7 +61,7 @@ class ShowTest(Resource):
         f = open('test','r+')
         r = f.read()
         f.close()
-        result = {'data': [("t: ", i)for i in r]}
+        result = {'text in file': [i[0] for i in r]}
         return jsonify(result)
 
 api.add_resource(Employees, '/employees') # Route_1
